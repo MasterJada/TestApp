c27efa897e6170ffccd617cf4402cb751f79bc80
olegreksha <masterjada@gmail.com>
2019-02-19 18:38:33 +0200

    Tab 1

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.testapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testapp.adapters.MainFragmentAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pager.adapter = MainFragmentAdapter(supportFragmentManager)
+        tab_layout.setupWithViewPager(pager, true)
     }
 }
---
+++ app/src/main/java/com/example/testapp/adapters/MainFragmentAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.testapp.adapters
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import com.example.testapp.tab_one.BlankFragment
+
+class MainFragmentAdapter(fm: FragmentManager?) : FragmentStatePagerAdapter(fm) {
+    override fun getItem(p0: Int): Fragment {
+        return  if(p0 == 0) BlankFragment.instance
+        else Fragment()
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return if(position == 0) "Tab 0" else "Tab 1"
+    }
+    override fun getCount() = 2
+}
